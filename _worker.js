4410116caeae29f074eef16ec7f7ca03bdfe645a
yanbf888 <yanbf888@gmail.com>
2024-07-27 00:45:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,8 +5,9 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
+// 3 Domains
 // const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
-const พร็อกซีไอพีs = ['workers.cloudflare.cyou'];
+const พร็อกซีไอพีs = [' '];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
